f61c0a8da182c11c5b867897cb1cd1124703036b
DL-Berkey <ruyria@naver.com>
2023-12-09 05:18:05 +0900

    글로벌 스타일 설정

---
+++ app/src/styles/GlobalStyle.tsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+}
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        font-family: Pretendard-Regular;
+    }
+
+    input, textarea {
+        font-family: Pretendard-Regular;
+    }
+
+    // 기본이 될 화면 사이즈
+    #root {
+        width: 100vw;
+        height: 961px;
+    }
+`;
+
+export default GlobalStyle;
