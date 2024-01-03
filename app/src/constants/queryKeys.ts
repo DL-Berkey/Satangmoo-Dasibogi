5c2d32d04eedd12ab940b6b2495852756523682d
DL-Berkey <ruyria@naver.com>
2024-01-03 23:39:15 +0900

    Query key 관리를 위한 constants/queryKeys.ts 파일 추가

---
+++ app/src/constants/queryKeys.ts
@@ -0,0 +1,9 @@
+export const QUERY_KEY = {
+    video: "video",
+    user_data: "userData",
+    bookmark_video_ids: "bookmark_video_ids",
+} as const;
+
+export const MUTATION_KEY = {
+    on_bookmarking: "on_bookmarking",
+};
